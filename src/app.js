ba31ee2ecd1b0059e9b8795ac7a715024aaa467b
bwitoradyo <bambang.witoradyo@gmail.com>
2017-09-25 21:25:08 -0400

    001 Rev

--- src/app.js
+++ src/app.js
@@ -1,8 +1,5 @@
 "use strict"
 //IMPORT REACT
-import React from "react";
-import {render} from "react-dom";
-import {Provider} from "react-redux";
 
 import {applyMiddleware, createStore} from "redux";
 import {logger} from "redux-logger";
@@ -16,12 +13,5 @@ const middleware = applyMiddleware(logger);
 const store = createStore(reducers, middleware);
 
 //IMPORT COMPONENTS
-import BooksList from "./components/pages/booksList";
-//RENDER THE COMPONEN
-render(
-  <Provider store={store}>
-    <BooksList/>
-  </Provider>, document.getElementById("app")	 
-);
 
-
+//RENDER THE COMPONENT
